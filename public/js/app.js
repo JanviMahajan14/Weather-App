5577bb75d8097b983ab83f584c32d8bb1866afa4
JanviMahajan14 <janvimahajan337@gmail.com>
2020-04-05 00:20:21 +0530

    Completed user interfacing

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,29 @@
-const result = async () => {
-    const response = await fetch('http://localhost:8000/weather?address=Jammu');
+const result = async (location) => {
+    const response = await fetch('http://localhost:8000/weather?address='+location);
     const data = await response.json();
+    if(data.error){
+        throw new Error(data.error);
+    }
     return data;
 }
 
-result().then((data)=>{
-    console.log(data)
-}).catch((error)=>{
-    console.log(error)
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+let messageOne = document.querySelector('.first');
+let messageSecond = document.querySelector('.second');
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const location = input.value;
+ 
+    messageOne.textContent = "Loading...";
+    messageSecond.textContent = "";
+
+    result(location).then((data)=>{
+        messageOne.textContent = data.location;
+        messageSecond.textContent = data.forcastData;
+    }).catch((error)=>{
+        messageOne.textContent = error.toString().replace("Error:","");
+        messageSecond.textContent = "";
+    })
 })
